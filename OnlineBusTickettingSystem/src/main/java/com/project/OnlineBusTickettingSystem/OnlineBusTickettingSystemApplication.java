4e24dc17d0acd79208d601e5f62ae0dd4099fe7a
Zocomon <sriparna1998@gmail.com>
2023-05-08 12:06:32 +0530

    initial commit

---
+++ OnlineBusTickettingSystem/src/main/java/com/project/OnlineBusTickettingSystem/OnlineBusTickettingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.OnlineBusTickettingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBusTickettingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBusTickettingSystemApplication.class, args);
+	}
+
+}
---
+++ OnlineBusTickettingSystem/src/test/java/com/project/OnlineBusTickettingSystem/OnlineBusTickettingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.OnlineBusTickettingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBusTickettingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
